7ea1d4742010d1bada397e8e3b7f256d472fcc20
pszczola08 <marceliszerszen08@gmail.com>
2025-03-10 09:55:09 +0100

    Third commit

--- app/src/main/java/com/example/zadanie4/MainActivity.kt
+++ app/src/main/java/com/example/zadanie4/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.zadanie4
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
 import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.Spinner
 import android.widget.TextView
@@ -25,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         val sugar = findViewById<CheckBox>(R.id.sugar_checkbox)
         val count = findViewById<SeekBar>(R.id.count)
         val countText = findViewById<TextView>(R.id.countText)
+        val mainCoffeeImage = findViewById<ImageView>(R.id.main_coffee_image)
+        val SubmitBtn = findViewById<Button>(R.id.btn_submit)
         count.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 countText.text = progress.toString()
@@ -33,5 +40,27 @@ class MainActivity : AppCompatActivity() {
             override fun onStartTrackingTouch(seekBar: SeekBar?) { }
             override fun onStopTrackingTouch(seekBar: SeekBar?) { }
         })
+        val typeImages = listOf(
+            R.drawable.szablon,
+            R.drawable.espresso,
+            R.drawable.americano,
+            R.drawable.latte,
+            R.drawable.capuccino,
+            R.drawable.flatwhite,
+            R.drawable.macciato,
+            R.drawable.mocca
+        )
+        coffee.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                mainCoffeeImage.setImageResource(typeImages[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { }
+        }
+
+        SubmitBtn.setOnClickListener {
+            val intent = Intent(this, OrderActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/zadanie4/OrderActivity.kt
@@ -0,0 +1,27 @@
+package com.example.zadanie4
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.order)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
